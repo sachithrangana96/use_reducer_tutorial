f6bc776381b6486764570554794403d4e63f4ce8
sachithrangana96 <84187564+sachithrangana96@users.noreply.github.com>
2023-05-02 12:24:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
+import { useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const reducer = (state,action)=>{
+ if(action.type==="setName"){
+  return {...state,name:action.payload};
+ }else if(action.type==="setCity"){
+  return {...state,city:action.payload};
+ }else if(action.type==="setPosition"){
+  return {...state,position:action.payload};
+ }else{
+  return state;
+ }
+}
+
 function App() {
+  //setState => dispatch function
+  const[state,setState] = useReducer(reducer,{
+    name:'',
+    city:'',
+    position:'',
+    dataArray:[],
+  })
+  console.log(state)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={state.name} onChange={(e)=>setState({
+        type:'setName',
+        payload:e.target.value
+      })} />
+      <input type="text" value={state.city} onChange={(e)=>setState({
+        type:'setCity',
+        payload:e.target.value
+      })} />
+      <input type="text" value={state.position} onChange={(e)=>setState({
+        type:'setPosition',
+        payload:e.target.value
+      })} />
+     <button  onClick={()=>setState({
+      type:"setArray",
+      payload:{
+        name:state.name,
+        city:state.city,
+        position:state.position
+      }
+      })}>Submit</button>
     </div>
   );
 }
